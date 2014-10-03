014d8b4db8b9b9bb5a1424fb5b29f57e3d1dfa4b
anberger <bergerandy@hotmail.com>
2014-10-03 13:07:41 +0200

    next change

--- AAD1_pro_emulator/src/com/aad1/aad1_pro_emulator/MainActivity.java
+++ AAD1_pro_emulator/src/com/aad1/aad1_pro_emulator/MainActivity.java
@@ -10,7 +10,7 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         
-        //fuck you andi
+        //fuck you michi
         
     }
 }
