a57dd6284f5f25d49bb283854647e7d5ea38ae5d
anberger <bergerandy@hotmail.com>
2014-10-03 13:31:09 +0200

    some changes

--- AAD1_pro_emulator/src/com/aad1/aad1_pro_emulator/MainActivity.java
+++ AAD1_pro_emulator/src/com/aad1/aad1_pro_emulator/MainActivity.java
@@ -11,6 +11,7 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
         
         //fuck you michi not askjdöfkjas
+        //alsjdöfljasd
         
     }
 }
