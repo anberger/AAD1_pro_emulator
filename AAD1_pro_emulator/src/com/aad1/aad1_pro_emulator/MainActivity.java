2ca73dcaae8cb6511e91a6558d00accae4b5393d
m1k337 <michael.pfister@gmx.at>
2014-10-03 13:34:27 +0200

    finally

--- AAD1_pro_emulator/src/com/aad1/aad1_pro_emulator/MainActivity.java
+++ AAD1_pro_emulator/src/com/aad1/aad1_pro_emulator/MainActivity.java
@@ -10,9 +10,7 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         
-        //fuck you michi not askjdöfkjas
-        //alsjdöfljasd
-        //so a schas
+        //finally it works, fucking bitches
         
     }
 }
