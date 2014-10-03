de510e1cfa48e9f977a3ca19f70ecb9e01740ea0
noelpb <noelpb9@gmail.com>
2014-10-03 13:37:39 +0200

    test mfckr

--- AAD1_pro_emulator/src/com/aad1/aad1_pro_emulator/MainActivity.java
+++ AAD1_pro_emulator/src/com/aad1/aad1_pro_emulator/MainActivity.java
@@ -11,6 +11,7 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
         
         //finally it works, fucking bitches
+        //testing motherfucker
         
     }
 }
