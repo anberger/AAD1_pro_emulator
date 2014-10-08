81ea91d28aa06c9c1edcce5c4bdbb71b967b4b37
m1k337 <michael.pfister@gmx.at>
2014-10-08 17:23:51 +0200

    client: first test + additions 1

--- AAD1_pro_emulator/src/com/aad1/aad1_pro_emulator/ClientActivity.java
+++ AAD1_pro_emulator/src/com/aad1/aad1_pro_emulator/ClientActivity.java
@@ -1,34 +1,38 @@
 package com.aad1.aad1_pro_emulator;
 
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 import java.net.InetAddress;
 import java.net.Socket;
 import java.net.UnknownHostException;
 import android.app.Activity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
 import android.os.Handler;
+import android.support.v4.content.LocalBroadcastManager;
 import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class ClientActivity extends Activity {
 	 
     private EditText serverIp;
- 
     private Button connectPhones;
- 
-    private static String serverIpAddress = "";
- 
-    private boolean connected = false;
- 
-    private Handler handler = new Handler();
-    
-    private static int SERVERPORT = 6000;
+    private Boolean connected = false;
+
+    TextView getMsg = null;
  
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,49 +42,38 @@ public class ClientActivity extends Activity {
         serverIp = (EditText) findViewById(R.id.server_ip);
         connectPhones = (Button) findViewById(R.id.connect_phones);
         connectPhones.setOnClickListener(connectListener);
+        getMsg = (TextView) findViewById(R.id.tvGet);
+        getMsg.setText("fuck you");
+        
+        LocalBroadcastManager.getInstance(this).registerReceiver(updateGUI, new IntentFilter("message"));
     }
+    
+    private BroadcastReceiver updateGUI = new BroadcastReceiver(){
+
+		@Override
+		public void onReceive(Context context, Intent intent) {
+			// TODO Auto-generated method stub
+			String message = intent.getStringExtra("msg");
+			
+			if(message != null){
+				getMsg.setText(message);
+			}
+		}
+    };
  
     private OnClickListener connectListener = new OnClickListener() {
  
         @Override
         public void onClick(View v) {
             if (!connected) {
-                serverIpAddress = serverIp.getText().toString();
-                if (!serverIpAddress.equals("")) {
-                    Thread cThread = new Thread(new ClientThread());
-                    cThread.start();
-                }
-            }
-        }
-    };
- 
-    public class ClientThread implements Runnable {
- 
-        public void run() {
-            try {
-                InetAddress serverAddr = InetAddress.getByName(ClientActivity.serverIpAddress);
-                Log.d("ClientActivity",serverAddr.toString());
-                Log.d("ClientActivity", "C: Connecting...");
-                Socket socket = new Socket(serverAddr, ClientActivity.SERVERPORT);
+            	ClientThread client = new ClientThread();
+            	client.setIp("192.168.1.11");
+            	Log.d("IP",client.getIp());
+                Thread cThread = new Thread(client);
+                
+                cThread.start();
                 connected = true;
-                //while (connected) {
-                    try {
-                        Log.d("ClientActivity", "C: Sending command.");
-                        PrintWriter out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socket
-                                    .getOutputStream())), true);
-                            // WHERE YOU ISSUE THE COMMANDS
-                            out.println("Hey Server!");
-                            Log.d("ClientActivity", "C: Sent.");
-                    } catch (Exception e) {
-                        Log.e("ClientActivity", "S: Error", e);
-                    }
-                //}
-                socket.close();
-                Log.d("ClientActivity", "C: Closed.");
-            } catch (Exception e) {
-                Log.e("ClientActivity", "C: Error"+ e, e);
-                connected = false;
             }
         }
-    }
+    };   
 }
